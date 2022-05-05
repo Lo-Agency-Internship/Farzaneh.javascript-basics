c1b65187ae7ea2d1ae17f85c68ef2419e01e2586
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-05-05 22:00:23 +0430

    function

--- JavaScript.js
+++ JavaScript.js
@@ -1,24 +1,24 @@
 
-alert("Hello everyone and welcome to this page! \n Please answer questions in the field");
+// alert("Hello everyone and welcome to this page! \n Please answer questions in the field");
 
-let  firstname =prompt ("Please enter your firstname!") 
-// prompt the user to put an input as a firstname
-let lastname=prompt ("Please enter your lastname!") 
-// prompt the user to put an input as the lastname
-let age=prompt ("Please enter your age!")
-//prompt the user to put an input as their age
-let gender=prompt ("Please enter your gender!")
-//prompt the user to put an input as their gender
+// let  firstname =prompt ("Please enter your firstname!") 
+// // prompt the user to put an input as a firstname
+// let lastname=prompt ("Please enter your lastname!") 
+// // prompt the user to put an input as the lastname
+// let age=prompt ("Please enter your age!")
+// //prompt the user to put an input as their age
+// let gender=prompt ("Please enter your gender!")
+// //prompt the user to put an input as their gender
 
 
-alert (
-    `name: ${firstname}`+" "+`lastname: ${lastname}`+" "+`age: ${age}`+" "+`gender: ${gender}`
-)
-// alert the user their info.
+// alert (
+//     `name: ${firstname}`+" "+`lastname: ${lastname}`+" "+`age: ${age}`+" "+`gender: ${gender}`
+// )
+// // alert the user their info.
 
-// ====================================
-// maths
-// part one: random integer
+// // ====================================
+// // maths
+// // part one: random integer
 
 function rand(x, y) {
     return Math.floor(Math.random() * ( x + y + 1))
@@ -27,7 +27,7 @@ console.log(rand(5, 7));
 
 
 
-// part two:  binary number
+// // part two:  binary number
 
 let binary = "010101"
 function bin2dec(str) {
@@ -43,9 +43,9 @@ function Sumtwointeger(x, y) {
     }
 }
 console.log(Sumtwointeger(2, 8))
-// =================================
-// part four about Comparisons
-//  1.compute the sum of the two given integers/If the two values are same, then returns triple their sum.
+// // =================================
+// // part four about Comparisons
+// //  1.compute the sum of the two given integers/If the two values are same, then returns triple their sum.
 function Sumtwointeger(x,y){
     if (x===y){
         return((x+y)*3)
@@ -54,8 +54,8 @@ function Sumtwointeger(x,y){
 
    console.log(Sumtwointeger(1,2))
 
-//    2. absolute difference between a specified number and 19/
-// Returns triple their absolute difference if the specified number is greater than 19.
+// //    2. absolute difference between a specified number and 19/
+// // Returns triple their absolute difference if the specified number is greater than 19.
 function cumputedifference(x){
     if (x > 19){
         return ((x-19)*3)
@@ -63,8 +63,8 @@ function cumputedifference(x){
 } 
 console.log(cumputedifference(20))
 
-//   3.create a new string adding "Lo" in front of a given string. 
-// If the given string begins with "Lo" then return the original string.
+// //   3.create a new string adding "Lo" in front of a given string. 
+// // If the given string begins with "Lo" then return the original string.
 function addstring(userstring){
     if (userstring.startsWith('Lo')){
         return(userstring)
@@ -75,22 +75,22 @@ function addstring(userstring){
    }
    console.log(addstring("Loagancy"));
 
-// =============================================  logical gates =================
-// Buffer false=> false/////true=>true
+// // =============================================  logical gates =================
+// // Buffer false=> false/////true=>true
 const x=true;
 function logicalGateBuffer(x) {
   return x;   
   }
 console.log(logicalGateBuffer(x));
 
-// Inverter false=> true/////true=> false
+// // Inverter false=> true/////true=> false
 const y=true;
 function logicalGateNot(y) {
   return y;   
   }
 console.log(logicalGateNot(y));
 
-// AND is like Multiplication 0/1====both True=> true else false
+// // AND is like Multiplication 0/1====both True=> true else false
 function logicalGateAnd(x, y)
 {
   if(x===true & y===true){
@@ -100,7 +100,7 @@ else return false;
 }
 console.log(logicalGateAnd(true,false));
 
-// OR is like sum0/1========both false=> false else true
+// // OR is like sum0/1========both false=> false else true
 function logicalGateOr(x, y)
 {
   if(x===false || y===false){
@@ -110,7 +110,7 @@ function logicalGateOr(x, y)
 }
 console.log(logicalGateOr(false,false));
 
-// NAND is like an invertion for AND
+// // NAND is like an invertion for AND
 function logicalGateNand(n, m)
 {
   if(n===true & m===true){
@@ -120,7 +120,7 @@ else return true;
 }
 console.log(logicalGateNand(false,true));
 
-// NOR is like an invertion for OR
+// // NOR is like an invertion for OR
 function logicalGateNor(n, m)
 {
   if(n===false & m===false){
@@ -130,7 +130,7 @@ else return false;
 }
 console.log(logicalGateNor(false,false));
 
-// XOR
+// // XOR
 function logicalGateXor(a, b)
 {
   if(a===false & b===false){
@@ -144,7 +144,7 @@ else return true;
 console.log(logicalGateXor(false,false));
 
 
-// XNOR
+// // XNOR
 function logicalGateXnor(a, b)
 {
   if(a===false & b===false){
@@ -161,8 +161,8 @@ console.log(logicalGateXnor(false,false));
 
 
 
-// =====================================LOOPS==================================
-// iterative: which should solve the factorial of x in an iterative way, and
+// // =====================================LOOPS==================================
+// // iterative: which should solve the factorial of x in an iterative way, and
 function factorialIterative(x){
   if (x > 0){
     let y= 1;
@@ -183,7 +183,7 @@ console.log(factorialIterative(x=5))
 
 
 
-// recursive: which should solve the factorial of x in a recursive way.
+// // recursive: which should solve the factorial of x in a recursive way.
 function factorialRecursive(x){
    if(x >0){
      let y=1
@@ -200,3 +200,73 @@ function factorialRecursive(x){
    }
 }
 console.log(factorialRecursive(x=6))
+
+// // ======================================Function======================================
+
+const arrRandMap= []
+arrRandMap.push(Math.floor(Math.random()*(50-15)+15));
+arrRandMap.push(Math.floor(Math.random()*(50-15)+15));
+arrRandMap.push(Math.floor(Math.random()*(50-15)+15));
+
+console.log(arrRandMap);
+// ===========================map=================
+function customMap(arr=[],callBack)
+{
+  const _value=[];
+  for(let val of arr)
+  {
+    _value.push(callBack(val));
+  }
+
+  return _value;
+}
+
+
+const op= customMap(arrRandMap,(val)=>{
+  return val*2;
+});
+console.log(op);
+
+
+// const arr=arrRandMap.map((item)=>
+// {return item-5
+// })
+// console.log(arr);
+
+
+// ============filter=========================
+function customFilter(arr=[],callBack)
+{
+  const _value=[];
+
+  for (let i=0 ; i<arr.length ; i++){
+    if (callBack (arr[i]))
+    {
+      _value.push (arr[i]);
+    
+    }
+  
+  }
+  return _value;
+}
+
+const upperThan= customFilter(arrRandMap,(val)=>{
+
+  return val>20;
+  
+})
+console.log(upperThan);
+// ======================reduce================
+
+function customReduce(arr, callback)
+{
+    let initial = arr[0];
+
+    for (let i = 1; i < arr.length; i++)
+    {
+        initial = callback(initial, arr[i], i);
+    }
+    return initial;
+}
+
+console.log(customReduce(arrRandMap, (acc, cv) => acc + cv));
